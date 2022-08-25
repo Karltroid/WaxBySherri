c564cc733e0dfabb511e87b2ce08be89a0fd5632
Karl <KarltroidContact@Gmail.com>
2022-08-24 20:10:18 -0400

    Initial project upload

---
+++ scripts/slideshow.js
@@ -0,0 +1,105 @@
+var currentslide, slideshow_leftbtn, slideshow_rightbtn, slideshow_photos, slideshow_leftcard, slideshow_centercard, slideshow_rightcard, autoslide;
+
+document.addEventListener("DOMContentLoaded", function(event) // wait for the webpage to finish loading
+{ 
+	autoslide = true;
+
+	slideshow_leftbtn = document.getElementById("slideshow_leftbtn");
+	slideshow_rightbtn = document.getElementById("slideshow_rightbtn");
+	slideshow_leftcard = document.getElementById("leftcard");
+	slideshow_centercard = document.getElementById("centercard");
+	slideshow_rightcard = document.getElementById("rightcard");
+
+	slideshow_photos = ["url('images/slideshow/image0.jpeg')",
+						"url('images/slideshow/image1.jpeg')",
+						"url('images/slideshow/image2.jpeg')",
+						"url('images/slideshow/image3.jpeg')",
+						"url('images/slideshow/image4.jpeg')",
+						"url('images/slideshow/image5.jpeg')",
+						"url('images/slideshow/image6.jpeg')",
+						"url('images/slideshow/image7.jpeg')",
+						"url('images/slideshow/image8.jpeg')",
+						"url('images/slideshow/image9.jpeg')",
+						"url('images/slideshow/image10.jpeg')",
+						"url('images/slideshow/image11.jpeg')",
+						"url('images/slideshow/image12.jpeg')"]; //findimages(); <-- implement when site is on server provider
+
+	currentslide = slideshow_photos.length - 1;
+
+	carousel();
+});
+
+
+function findimages()
+{
+	let foundall = false;
+	let imgCount = 0;
+	let images = [];
+
+	for (let currentImg = 0; currentImg <= imgCount; currentImg++)
+	{
+		let img = document.createElement('img');
+		let imgurl = "url('images/slideshow/image" + imgCount + ".jpeg')"
+		img.src = imgurl;
+		console.log("found " + imgurl)
+		img.onload = function(e)
+		{
+			images.push(imgurl);https://drive.google.com/drive/folders/1FaAX1urlKGQs9zWtsplUQfLwuLj9mV24?usp=sharing
+			imgCount++;
+			;
+		};
+
+		document.body.appendChild(img);
+		img.remove();
+	}
+
+	return images;
+}
+
+
+var slideshow_timeouts = [];
+
+function carousel(autoslide = true, increment = 1)
+{
+	currentslide = arrayrange(currentslide + increment, 0, slideshow_photos.length);
+
+	slideshow_leftcard.classList.remove('slideshowanimation_leftright');
+	slideshow_centercard.classList.remove('slideshowanimation_center');
+	slideshow_rightcard.classList.remove('slideshowanimation_leftright');
+
+	slideshow_leftcard.style.backgroundImage = slideshow_photos[arrayrange(currentslide + 1, 0, slideshow_photos.length)] + ", " + slideshow_photos[arrayrange(currentslide + 2, 0, slideshow_photos.length)];
+	slideshow_centercard.style.backgroundImage = slideshow_photos[currentslide] + ", " + slideshow_photos[arrayrange(currentslide + 1, 0, slideshow_photos.length)];
+	slideshow_rightcard.style.backgroundImage = slideshow_photos[arrayrange(currentslide - 1, 0, slideshow_photos.length)] + ", " + slideshow_photos[currentslide];
+
+	if (autoslide)
+	{
+		slideshow_timeouts.push(setTimeout(function() {
+			slideshow_leftcard.classList.add('slideshowanimation_leftright');
+			slideshow_centercard.classList.add('slideshowanimation_center');
+			slideshow_rightcard.classList.add('slideshowanimation_leftright');
+		}, 2500));
+		slideshow_timeouts.push(setTimeout(carousel, 3500));
+	}
+	else
+	{
+		for (let timeout = 0; timeout < slideshow_timeouts.length; timeout++) {
+			clearTimeout(slideshow_timeouts[timeout]);
+		}
+		slideshow_timeouts = [];
+		slideshow_timeouts.push(setTimeout(function() {
+			carousel();
+		}, 20000));
+	}
+}
+
+
+function arrayrange(current, min, max)
+{
+	if (current > max - 1)
+		current = current - max;
+	else if (current < min)
+		current = current + max;
+
+	return current;
+}
+
---
+++ scripts/stickynavbar.js
@@ -0,0 +1,63 @@
+var header, navbar, mobile_nav, mobile_nav_btn; // page elements
+var progress = 0;  // integers
+var mobile_nav_open = false;
+
+window.addEventListener("load", function()
+{
+	// get the variables and update the navbar when the page finishes loading
+	header = document.getElementById("HeaderSection");
+	navbar = document.getElementById("navbar");
+	mobile_nav = document.getElementById("mobilenavitems");
+	mobile_nav_btn = document.getElementById("mobilenav_btn")
+
+	update_navbar();
+});
+
+
+document.addEventListener('scroll', function()
+{
+	// update nav bar when the user is scrolling
+	update_navbar();
+});
+
+window.addEventListener('resize', function()
+{
+	console.log("pog");
+	if (window.innerWidth <= 990 && mobile_nav_open)
+		toggle_mobile_nav();
+});
+
+
+function update_navbar() // make navbar fixed to top of the page after it is scrolled to
+{
+	if (header == undefined)
+		return;
+	
+	progress = window.scrollY/(header.clientHeight-navbar.clientHeight);  // (0 to 1) - % of the header scrolled through
+
+	navbar.style.opacity = progress;
+
+	if (progress >= 1)
+		navbar.classList.add("sticky-navbar");
+	else
+		navbar.classList.remove("sticky-navbar");
+}
+
+function toggle_mobile_nav()
+{
+	if (mobile_nav_open)
+	{
+		mobile_nav_open = false;
+		mobile_nav.style.display = "none";
+		document.body.classList.remove("stop-scrolling");
+		mobile_nav_btn.src = "images/icons/menu.png";
+		return;
+	}
+
+	mobile_nav_open = true;
+	mobile_nav.style.display = "block";
+	document.body.classList.add("stop-scrolling");
+	mobile_nav_btn.src = "images/icons/close.png";
+	if (window.scrollY < header.clientHeight - navbar.clientHeight)
+		window.scrollTo(0, header.clientHeight - navbar.clientHeight);
+}
